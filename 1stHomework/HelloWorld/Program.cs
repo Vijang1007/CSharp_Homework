2032171e5d0d6e288244869ca51846558bbc4a70
Vijang1007 <109446727+Vijang1007@users.noreply.github.com>
2022-07-20 22:26:14 +0900

    2nd Homework
    Done

--- 1stHomework/HelloWorld/Program.cs
+++ 1stHomework/HelloWorld/Program.cs
@@ -10,7 +10,17 @@ namespace HelloWorld
     {
         static void Main()
         {
-            System.Console.WriteLine("Hello World");
+            string s1, s2;
+            System.Console.Write("input s1 :");
+            s1 = System.Console.ReadLine();
+            System.Console.Write("input s2 :");
+            s2 = System.Console.ReadLine();
+            System.Console.WriteLine($"the string is\n{s1}, {s2}");
+            System.Console.WriteLine("the string is\n{0}, {1}", s1, s2);
+            string message;
+            message = s1 + ", " + s2;
+            System.Console.WriteLine("the string is\n{0}", message);
+            System.Console.WriteLine($"the string is\n{message}");
             System.Console.ReadKey();
         }
     }
